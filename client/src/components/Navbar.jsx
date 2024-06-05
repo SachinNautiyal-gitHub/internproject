2a0dd9e6024e3093520c9e9f0b7e5c76bea59083
sachinnautiyal-github <nautiyalsachin217@gmail.com>
2024-06-05 16:17:57 +0530

    changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,41 +1,69 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './styles/navbar.css'
 import { Link } from 'react-router-dom'
 import avatar from '../asset/avatar.jpg'
 
 const Navbar = (props) => {
-   
-   
-   if (props.user.length > 0){
+  
+
+   const [isMenuOpen, setMenuOpen] = useState(false);
+
+   const toggleMenu = () => {
+     setMenuOpen(!isMenuOpen);
+   };
+
+   if (props.user.length > 0) {
       return (
          <>
-         <div className="navbar">
-            <div className="title">
-               <h2>Acme Co</h2>
+            <div className={`navbar ${isMenuOpen ? 'menuopen' : ""}`}>
+               <div className="title">
+                  <h2>Acme Co</h2>
+               </div>
+
+               <div className="nav-list">
+                  <ul>
+                     <Link className="link" to={'/'}>Home</Link>
+                     <Link className="link" >Book</Link>
+                     <Link className="link">Guests</Link>
+                     <Link className="link">Events</Link>
+                     <Link className="link">Services</Link>
+                     <Link className="link" >Support</Link>
+                  </ul>
+                  <div className="avatar">
+                     <Link to={'/profile'}> <img src={props.user[0].profileImg || avatar} alt="logo" /></Link>
+                  </div>
+               </div>
+
+               <div className="menuIcon" onClick={toggleMenu}>
+                  <div className="bar"></div>
+                  <div className="bar"></div>
+                  <div className="bar"></div>
+               </div>
+
+               {isMenuOpen && (
+
+                  <div className="hamburgermenu">
+                     <ul >
+
+                     <Link className="link" to={'/'}>Home</Link>
+                     <Link className="link" >Book</Link>
+                     <Link className="link">Guests</Link>
+                     <Link className="link">Events</Link>
+                     <Link className="link">Services</Link>
+                     <Link className="link" >Support</Link>
+                     <Link className="link" to={'/profile'} >Profile</Link>
+
+                     </ul>
+                  </div>
+               )}
+
+
             </div>
-       
-            <div className="nav-list">
-               <ul>
-                   <Link className="link" to={'/'}>Home</Link> 
-                   <Link className="link" >Book</Link>
-                   <Link className="link">Guests</Link>
-                   <Link className="link">Events</Link>
-                   <Link className="link">Services</Link>
-                   <Link className="link" >Support</Link>
-               </ul>
-               <div className="avatar">
-                  <Link to={'/profile'}> <img src={props.user[0].profileImg || avatar} alt="logo" /></Link>
-             </div>
-            </div>
-       
-          
-       
-         </div> 
          </>
-         )
+      )
    }
 
- 
+
 }
 
 
